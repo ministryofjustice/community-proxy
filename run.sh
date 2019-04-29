54d497bf8f2bf67220f3143bdba8b9b7329b0aca
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2019-04-29 16:22:06 +0100

    location of key store in docker image

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# JAVA_OPTS="-Djavax.net.ssl.trustStore=trusted.jks -Djavax.net.ssl.trustStorePassword=${TRUST_STORE_PASSWORD} -Djavax.net.ssl.trustStoreType=jks"
-JAVA_OPTS="-Djavax.net.ssl.trustStore=trusted.jks -Djavax.net.ssl.trustStorePassword=D0N0tT3ll -Djavax.net.ssl.trustStoreType=jks"
+# JAVA_OPTS="-Djavax.net.ssl.trustStore=/app/trusted.jks -Djavax.net.ssl.trustStorePassword=${TRUST_STORE_PASSWORD} -Djavax.net.ssl.trustStoreType=jks"
+JAVA_OPTS="-Djavax.net.ssl.trustStore=/app/trusted.jks -Djavax.net.ssl.trustStorePassword=D0N0tT3ll -Djavax.net.ssl.trustStoreType=jks"
 
 exec java -XX:+PrintFlagsFinal ${JAVA_OPTS} \
   -Dcom.sun.management.jmxremote.local.only=false \
