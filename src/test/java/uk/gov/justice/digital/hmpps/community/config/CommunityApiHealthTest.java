99aba4755be0846ff4c1fa03071011b48599ad4e
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2019-06-24 12:11:22 +0100

    DT-8: Updated Dependency Health endpoint call to /ping And Updated spring boot version

---
+++ src/test/java/uk/gov/justice/digital/hmpps/community/config/CommunityApiHealthTest.java
@@ -0,0 +1,66 @@
+package uk.gov.justice.digital.hmpps.community.config;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.MockRestServiceServer.bindTo;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CommunityApiHealthTest {
+
+    private MockRestServiceServer mockCommunityAPI;
+
+    private TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    @Qualifier("deliusApiHealthRestTemplate")
+    private RestTemplate restTemplate;
+
+    @Before
+    public void setup() throws IOException {
+        mockCommunityAPI = buildMockService(restTemplate);
+        mockCommunityAPI
+                .expect(requestTo("http://localhost:8099/api/health"))
+                .andRespond(withSuccess());
+    }
+
+    private MockRestServiceServer buildMockService(RestTemplate restTemplate) {
+        MockRestServiceServer.MockRestServiceServerBuilder infoBuilder = bindTo(restTemplate);
+        infoBuilder.ignoreExpectOrder(true);
+        return infoBuilder.build();
+    }
+
+    @Test
+    public void healthEndpointCallsCommunityAPIPing() {
+
+        ResponseEntity<String> response = testRestTemplate.getForEntity(
+                getBasePath() + "communityapi/health", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("{\"status\":\"UP\"}");
+    }
+
+    private String getBasePath() {
+        return "http://localhost:" + port;
+    }
+}
